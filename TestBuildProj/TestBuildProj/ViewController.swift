e2180a112f258e245dfb6ce4d5ea2e0687d9ab1b
Michael Wasserman <michael.wasserman@traderev.com>
2019-05-29 22:01:21 -0400

    - project creation

---
+++ TestBuildProj/TestBuildProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBuildProj
+//
+//  Created by Michael Wasserman on 2019-05-29.
+//  Copyright © 2019 Michael Wasserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
