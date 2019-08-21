8cf25700ab80664cee246ba6737c8e978806b76d
Michael Wasserman <michael.wasserman@traderev.com>
2019-08-21 16:29:31 -0400

    - Added Array Extension

---
+++ TestBuildProj/TestBuildProj/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Extensions.swift
+//  TestBuildProj
+//
+//  Created by Michael Wasserman on 2019-08-21.
+//  Copyright © 2019 Michael Wasserman. All rights reserved.
+//
+
+import Foundation
+
+public extension Array {
+
+    /// SwifterSwift: Safely swap values at given index positions.
+    ///
+    ///        [1, 2, 3, 4, 5].safeSwap(from: 3, to: 0) -> [4, 2, 3, 1, 5]
+    ///        ["h", "e", "l", "l", "o"].safeSwap(from: 1, to: 0) -> ["e", "h", "l", "l", "o"]
+    ///
+    /// - Parameters:
+    ///   - index: index of first element.
+    ///   - otherIndex: index of other element.
+    mutating func safeSwap(from index: Index, to otherIndex: Index) {
+        guard index != otherIndex else { return }
+        guard startIndex..<endIndex ~= index else { return }
+        guard startIndex..<endIndex ~= otherIndex else { return }
+        swapAt(index, otherIndex)
+    }
+
+}
